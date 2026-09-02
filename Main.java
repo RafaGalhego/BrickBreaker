84d21ca619f28a8d6bbaa66218befbdd8a5f1097
RafaGalhego <rafagalhego@hotmail.com>
2026-09-02 14:54:08 -0300

    Configurações iniciais
    Criação da tela do jogo e timer para atualização em tempo real.

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        JFrame janela = new JFrame("Brick Breaker");
+        PainelJogo painel = new PainelJogo();
+
+        janela.add(painel);
+        janela.setSize(800, 600);
+        janela.setResizable(false);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+}
---
+++ PainelJogo.java
@@ -0,0 +1,35 @@
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PainelJogo extends JPanel implements ActionListener {
+
+    private Timer timer;
+
+    public PainelJogo() {
+        setBackground(Color.BLACK);
+
+        timer = new Timer(16, this); //ajuste para 60 FPS
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); //limpar a tela
+
+        //desenha a tela inicial
+        g.setColor(Color.WHITE);
+        g.drawString("Brick Breaker!", 50, 50);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //mover objetos
+        //checar colisões
+
+        repaint();  //feitas as movimentações e verificações, redesenha o painel em tempo real
+    }
+}
